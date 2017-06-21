961977f3c9429807be9ce18958ee9dc119cd76c4
rdannyr787 <rdannyr787@gmail.com>
2017-06-21 16:18:58 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 ﻿<?php
-include ("views/header.php");
 
-include ("views/ocr.php");
+	include("views/header.php");
+	
+	if ($_GET['page'] ==  'tips.php' ){
+		include("views/tips.php");
+	}
+	else if ($_GET['page'] ==  'about.php' ){
+		include("views/about.php");
+	}
+	else{
+	include("views/ocr.php");
+	}
 
-include ("views/footer.php");
+	include("views/footer.php");
+
 ?>
