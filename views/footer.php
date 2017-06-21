697266ce9f96f5a9cb8738c343859d2347b9d7eb
rdannyr787 <rdannyr787@gmail.com>
2017-06-21 16:41:14 -0400

    Update footer.php

--- views/footer.php
+++ views/footer.php
@@ -36,10 +36,7 @@ foot
                 readURL(this);
             });
 			
-			$(".nav li").on("click", function() {
-      $(".nav li").removeClass("active");
-      $(this).addClass("active");
-    });
+  
 			    
         </script>
 
